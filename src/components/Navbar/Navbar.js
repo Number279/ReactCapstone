b384c6595a5b05fd5f0719697ac4750f8d9ec16c
Number279 <sawyercarroll@gmail.com>
2021-03-21 20:58:24 -0500

    Create NavBar Styled

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class NavBar extends Component{
 render(){
     return(
-    <div className="nav">
-        <Link to = "/">Home  </Link>
-        {/* <Link to = "/about">  About Find Help Here  </Link> */}
-        <Link to = "/aboutus"> About the Developers</Link> 
-    </div>
+        <div>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Find Help Here</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/aboutus">About the Devs</Nav.Link>
+          {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+        </Nav>
+        {/* <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form> */}
+      </Navbar>
+      </div>
         );
     }
 }
 
 export default NavBar
+
